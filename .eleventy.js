c3aa6357adddf55d392104b301fb7b8b3023d20b
Wolf-dot <wojciech.lukasz.belka@protonmail.com>
2022-05-31 16:30:23 +0200

    fix

--- .eleventy.js
+++ .eleventy.js
@@ -183,18 +183,6 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addCollection("posts", function(collection) {
     const coll = collection.getFilteredByTag("posts").sort((a,b) => b.data.order - a.data.order);
     for (let i=0; i < coll.length; i++){
-      const nextPost = coll[i - 1];
-      const prevPost = coll[i + 1];
-
-      coll[i].data["prevPost"] = prevPost;
-      coll[i].data["nextPost"] = nextPost;
-    }
-    return coll;
-  });
-
-  eleventyConfig.addCollection("postsOrder", function(collection) {
-    const coll = collection.getFilteredByTag("posts").sort((a,b) => b.data.order - a.data.order);
-    for (let i=0; i < coll.length; i++){
       const prevPost = coll[i - 1];
       const nextPost = coll[i + 1];
 
